a8b2fd3c5775eb7a0e76a91ff899444b77e86317
AlexTello0695 <atello0695@gmail.com>
2026-07-23 19:22:01 -0600

    Funcionalidad y diseño de CartSidebar finalizados

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,14 +1,12 @@
 import { ArrowUpRightIcon, BadgePercentIcon, BikeIcon, ChevronDownIcon, LogOutIcon, MapPinIcon, MenuIcon, PackageIcon, SearchIcon, ShieldIcon, ShoppingCartIcon, UserIcon, XIcon } from "lucide-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 
 const NavBar = () => {
 
   const user: any = {name: "Alex", email: "alext@ejemplo.com", isAdmin: "true"}
-  const {cartCount, setIsCartOpen} = {
-    cartCount: 10,
-    setIsCartOpen: (_data: any)=> {}
-  };
+  const {cartCount, setIsCartOpen} = useCart()
   const [searchQuery, setSearchQuery] = useState("")
   const [userMenuOpen, setUserMenuOpen] = useState(false)
   const navigate = useNavigate()
